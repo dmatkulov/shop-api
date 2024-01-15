ca46be0df86ba08bc7d08996bc01ae4acdecdfa2
Dilshad Matkulov <dilshadmatkulov@gmail.com>
2024-01-15 20:48:31 +0600

    lesson 75

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import productsRouter from './routers/products';
+import cors from 'cors';
 import fileDb from './fileDb';
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/products', productsRouter);
 
@@ -15,6 +17,6 @@ const run = async () => {
   app.listen(port, () => {
     console.log(`Server started on ${port} port`);
   });
-}
+};
 
 run().catch(console.error);
